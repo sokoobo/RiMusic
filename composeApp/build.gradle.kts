d1c684ee7f63861d44ef02bead85d6a52ba37295
Rino Russo <rinorusso@gmail.com>
2025-02-23 15:36:05 +0100

    Updated languages

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -75,6 +75,7 @@ kotlin {
             implementation(libs.newpipe.extractor)
             implementation(libs.nanojson)
             implementation(libs.androidx.webkit)
+            implementation(libs.dotenv)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
